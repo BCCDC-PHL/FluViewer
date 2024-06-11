a06ca11f63e1a89a1ab3f96c943610ef432a5f37
Dan Fornika <dfornika@gmail.com>
2024-06-11 12:57:21 -0700

    check that all tests pass, exit with error code if not

--- .github/scripts/check_outputs.sh
+++ .github/scripts/check_outputs.sh
@@ -6,3 +6,10 @@
     --outdir artifacts
 
 column -ts ',' artifacts/check_outputs_summary.csv
+
+while read -r test_name test_result; do
+    if [ "$test_result" == "FAIL" ]; then
+	echo "Test $test_name failed"
+	exit 1
+    fi
+done < tail -n+2 artifacts/check_outputs_summary.csv
