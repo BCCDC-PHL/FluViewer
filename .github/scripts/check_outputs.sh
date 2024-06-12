4bc626b0badafe3d1151c6b3e7a486ed80dd91e4
Dan Fornika <dfornika@gmail.com>
2024-06-12 09:40:22 -0700

    clear out gh-pages

--- .github/scripts/check_outputs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-.github/scripts/check_outputs.py \
-    --analysis-outdir-upstream .github/data/test_output/KevinKuchinski-FluViewer-output \
-    --analysis-outdir-origin .github/data/test_output/BCCDC-PHL-FluViewer-output \
-    --outdir artifacts
