c9a19dfafe0437a04beabfacce1b5a8770f0f9d7
Dan Fornika <dfornika@gmail.com>
2024-07-10 16:45:33 -0700

    Fix fluviewer installation in consistency check workflow

--- .github/scripts/install_fluviewer_bccdc-phl.sh
+++ .github/scripts/install_fluviewer_bccdc-phl.sh
@@ -4,7 +4,7 @@ source ${HOME}/.bashrc
  
 eval "$(conda shell.bash hook)"
 
-conda env create -f environment.yaml -n fluviewer-bccdc-phl
+conda env create -f .github/environments/fluviewer-bccdc-phl.yml -n fluviewer-bccdc-phl
 
 conda activate fluviewer-bccdc-phl
 
